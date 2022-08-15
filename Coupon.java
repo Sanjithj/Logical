ffbdfe1c0f1aa171481ea594bffa6da23dfb6d05
Sanjithj <sanjithj591997@gmail.com>
2022-08-15 13:44:36 +0530

    Added: Coupon code

---
+++ Coupon.java
@@ -0,0 +1,29 @@
+package com.bridgelabz;
+
+public class Coupon {
+	static void printUnique(int l, int r)
+    {
+        for (int i=l ; i<=r ; i++)
+        {
+            int num = i;
+            boolean visited[] = new boolean[10];
+            while (num != 0)
+            {
+                if (visited[num % 10])
+                    break;
+      
+                visited[num%10] = true;
+      
+                num = num/10;
+            }
+            if (num == 0)
+                System.out.print(i + " ");
+        }
+    }
+     
+    public static void main(String args[])
+    {
+        int l = 1, r = 20;
+        printUnique(l, r);
+    }
+}
